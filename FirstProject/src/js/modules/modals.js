a91ef43147d39b986f3adb0b0e55b5e10b43dbd2
Максим Сергеев <msergeev@MacBook-Pro-Maksim.local>
2022-07-24 16:59:17 +0300

    [FP-5] update: добавление функциональности табов и переключение модальных окон

--- FirstProject/src/js/modules/modals.js
+++ FirstProject/src/js/modules/modals.js
@@ -1,24 +1,37 @@
 const modals = () => {
-    function bindModal(triggSelector, modalSelector, closeSelector) {
+    function bindModal(triggSelector, modalSelector, closeSelector, closeClickOverlay = true) {
         const trigger = document.querySelectorAll(triggSelector);
         const modal = document.querySelector(modalSelector);
         const close = document.querySelector(closeSelector)
+        const windows = document.querySelectorAll('[data-modal]')
 
         trigger.forEach(item => {
             item.addEventListener('click', (e) => {
                 if (e.target) {
                     e.preventDefault();
                 }
+                //закрытие все ненужных модалок
+                windows.forEach((item) => {
+                    item.style.display = 'none';
+                })
                 modal.style.display = "block";
                 document.body.style.overflow = "hidden";
             })
         })
         close.addEventListener('click', () => {
+            //закрытие все ненужных модалок
+            windows.forEach((item) => {
+                item.style.display = 'none';
+            })
             modal.style.display = "none";
             document.body.style.overflow = "";
         })
         modal.addEventListener('click', (e) => {
-            if (e.target === modal) {
+            if (e.target === modal && closeClickOverlay) {
+                //закрытие все ненужных модалок
+                windows.forEach((item) => {
+                    item.style.display = 'none';
+                })
                 modal.style.display = "none";
                 document.body.style.overflow = "";
             }
@@ -34,6 +47,9 @@ const modals = () => {
 
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
+    bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close', false);
+    bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
+    bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
     showModalByTime('.popup', 10000);
 }
 
